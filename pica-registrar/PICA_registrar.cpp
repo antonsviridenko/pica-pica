7f9ada392430df8b11ee198634b3fbff73276bcb
me_desk <me_desk@fb96df88-216d-11de-830c-f3023f12d8ac>
2012-07-09 20:11:09 +0000

    registrar, continued
    git-svn-id: svn://localhost/project_picapica/trunk@142 fb96df88-216d-11de-830c-f3023f12d8ac

--- pica-registrar/PICA_registrar.cpp
+++ pica-registrar/PICA_registrar.cpp
@@ -10,7 +10,10 @@
 #include <unistd.h>
 #include <sys/errno.h>
 
+#include <cstring>
 #include <map>
+
+using namespace std;
 
 #define PICA_REGISTRAR_PORT 2299
 #define READ_BUF_SIZE 4096
@@ -43,9 +46,10 @@ while (1)
 	if (client_socket >= 0)
 		{
 		 pos = 0;
+		 disconnect_flag = 0;
 		 do 
 			{
-			 ret = recv(client_socket, read_buf + pos, READ_BUF_SIZE - pos, 0);
+			 ret = recv(client_socket, read_buf + pos, READ_BUF_SIZE - 1 - pos, 0);
 			 
 			 if (ret > 0)
 				pos += ret;
@@ -55,10 +59,46 @@ while (1)
 			 	if (ret < 0)
 					perror("recv");
 			 	}
-			 
+			 read_buf[pos + 1] = '\0';
 			}
-		while();
+		while(pos < READ_BUF_SIZE && !disconnect_flag && !std::strstr(read_buf, "-----END CERTIFICATE REQUEST-----"));
+		
+		if (disconnect_flag)
+			continue;
+		
+		{
+		FILE *csr_temp;
+		
+		std::string filename = (string("/tmp/CSR-") + inet_ntoa(sc.sin_addr) );
+		
+		csr_temp = fopen(filename.c_str, "w");
+		
+		if (!csr_temp)
+			{
+			perror("fopen");
+			continue;
+			}
+		
+		ret = fwrite(read_buf, pos, 1, csr_temp);
+		
+		if (ret != 1)
+		{
+			perror("fwrite");
+			fclose(csr_temp);
+			remove(filename.c_str);
+			continue;
 		}
+		
+		fclose(csr_temp);
+		
+		//system(openssl ca)
+		
+		remove(filename.c_str);
+		}
+		
+		}
+	else
+		perror("accept");
 
 	}
 
