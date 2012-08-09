cc2105b2b261f557838fec640d201d93f4c89665
me_laptop <me_laptop@fb96df88-216d-11de-830c-f3023f12d8ac>
2012-08-09 19:50:09 +0000

    default installation prefix
    git-svn-id: svn://localhost/project_picapica/trunk@158 fb96df88-216d-11de-830c-f3023f12d8ac

--- PICA_nodeconfig.h
+++ PICA_nodeconfig.h
@@ -2,13 +2,15 @@
 #ifndef PICA_NODECONFIG_H
 #define PICA_NODECONFIG_H
 
-//FIXME these default values should be set by "configure" script
-//FIXME put these macros inside man pages and substitute them using sed during "make install"
+#ifndef PICA_PREFIX
+#define PICA_PREFIX "/usr"
+#endif
+
 #define PICA_NODECONFIG_DEF_ANNOUNCED_ADDR "0.0.0.0"
-#define PICA_NODECONFIG_DEF_LISTEN_PORT 2299
+#define PICA_NODECONFIG_DEF_LISTEN_PORT "2299"
 #define PICA_NODECONFIG_DEF_NODES_DB_FILE "/var/lib/pica-node/nodelist.db"
 #define PICA_NODECONFIG_DEF_CONFIG_FILE "/etc/pica-node.conf"
-#define PICA_NODECONFIG_DEF_CA_CERT_FILE "/usr/share/pica-node/CA.pem"
+#define PICA_NODECONFIG_DEF_CA_CERT_FILE PICA_PREFIX"/share/pica-node/CA.pem"
 
 struct nodeconfig
 {
