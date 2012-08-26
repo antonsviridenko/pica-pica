4783c479d911838e0cb0a545140d7a7a70578a57
me_desk <me_desk@fb96df88-216d-11de-830c-f3023f12d8ac>
2012-08-26 20:19:30 +0000

    version bump to 0.5.2; new seed node addreses;Gentoo ebuild and initscript included in dist
    git-svn-id: svn://localhost/project_picapica/trunk@189 fb96df88-216d-11de-830c-f3023f12d8ac

--- nodelist_db_create.sql
+++ nodelist_db_create.sql
@@ -8,6 +8,11 @@ create table if not exists nodes
                         inactive_count int not null,
                         constraint pk primary key (address,port) on conflict replace 
                      );
-insert into nodes values("picapica.im", 2299, 0, 0); 
-insert into nodes values("picapica.ge", 2299, 0, 0); 
-
+insert into nodes values("picapica.im", 2299, 0, 0);  
+insert into nodes values("91.202.255.68", 2299, 0, 0);
+insert into nodes values("89.237.21.144", 2299, 0, 0);
+insert into nodes values("89.31.112.34", 2299, 0, 0);
+insert into nodes values("77.81.242.16", 2299, 0, 0);
+insert into nodes values("159.253.18.46", 2299, 0, 0);
+insert into nodes values("68.36.120.200", 2299, 0, 0);
+insert into nodes values("picapica.ge", 2299, 0, 0);
