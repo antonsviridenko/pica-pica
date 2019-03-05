032eb1f23992568e3ddd7946f94cd69a464167ba
Anton <anton@picapica>
2019-03-05 22:33:35 +0400

    add stddef.h, prevents compilation failure without miniupnpc, defines NULL

--- PICA_netconf.c
+++ PICA_netconf.c
@@ -16,6 +16,7 @@
 	along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 #include "PICA_netconf.h"
+#include <stddef.h>
 
 #ifdef HAVE_LIBMINIUPNPC
 #include <miniupnpc/miniupnpc.h>
