24f85e400476f5add7ffa9fc40629339ac840324
Anton <anton@picapica>
2019-03-06 22:54:26 +0400

    remove unreachable nodes

--- nodelist_db_create.sql
+++ nodelist_db_create.sql
@@ -24,7 +24,5 @@ create table if not exists nodes
                         constraint pk primary key (address,port) on conflict replace 
                      );
 insert into nodes values("picapica.im", 2233, 0, 0);
-insert into nodes values("159.253.18.46", 2299, 0, 0);
 insert into nodes values("picapica.im", 2299, 0, 0);  
-insert into nodes values("89.31.112.34", 2299, 0, 0);
 insert into nodes values("picapica.ge", 2299, 0, 0);
