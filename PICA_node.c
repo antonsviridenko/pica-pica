9302dc71e5efe8915d18e88a2e3e9567a7f8111a
Anton <anton@picapica>
2019-11-13 18:42:36 +0400

    fix license texts displayed by pica-node and pica-client in runtime

--- PICA_node.c
+++ PICA_node.c
@@ -3452,7 +3452,7 @@ void print_version()
 {
 	puts("Pica Pica Node v"PACKAGE_VERSION" \nCopyright  (c) 2012 - 2018 Anton Sviridenko\n");
 	puts("protocol version "PICA_PROTO_VER_STRING"\n");
-	puts("License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>\n\
+	puts("License GPLv3: GNU GPL version 3 <http://gnu.org/licenses/gpl.html>\n\
 This is free software: you are free to change and redistribute it.\n\
 There is NO WARRANTY, to the extent permitted by law.\n");
 
